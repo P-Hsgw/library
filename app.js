ba1e8e0035be155585161ed9e4752f7e10eed822
P-Hsgw <contact@p-hsgw.com>
2021-03-26 23:28:21 +0900

    Add a function that will let user add books, and a function that will display books

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const display = document.getElementById("display")
+const title = document.getElementById("title")
+const author = document.getElementById("author")
+const pages = document.getElementById("pages")
+const read = document.getElementById("read")
+
+
+
+
+let myLibrary = [];
+
 function Book (title, author, pages, read) {
   this.title = title
   this.author = author
@@ -7,3 +18,23 @@ function Book (title, author, pages, read) {
     return `${title} by ${author}, ${pages} pages, ${(read) ? `read already`: `not read yet`} `
   }
 }
+
+function addToLibrary(title, author, pages, read) {
+  let newBook = new Book (title, author, pages, read)
+  myLibrary.push(newBook)
+}
+
+function displayBooks() {
+  myLibrary.forEach (function (book) {
+    title.innerHTML = book.title;
+    author.innerHTML = book.author;
+    pages.innerHTML = book.pages;
+    read.innerHTML = book.read;
+  })
+}
+
+addToLibrary("The Lord of the Rings", "J. R. R. Tolkien", 1000, true)
+displayBooks()
+console.log(myLibrary)
+
+// Create new card for each book when displaying Books - each with data attribute corresponding to array index
